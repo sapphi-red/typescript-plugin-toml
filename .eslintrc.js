6da801a678d4eaf09f29bd7d000c436b72a8aea3
sapphi-red <green@sapphi.red>
2021-11-15 00:47:40 +0900

    Chore: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint'
+    'plugin:prettier/recommended'
   ],
   rules: {
     'no-console': 'warn',
