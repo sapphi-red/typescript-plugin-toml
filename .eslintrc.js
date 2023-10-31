9ce1387f3dd040f4f84feba7b9ec9580e43112a0
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-10-31 22:06:07 +0900

    chore: remove eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    'prettier'
   ],
   rules: {
     'no-console': 'warn',
